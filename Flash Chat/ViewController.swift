82fd125aeb5f602070bbb88b72f3fa078ee6f894
Mykyta Havrylenko <gavverment@gmail.com>
2022-07-11 13:26:10 +0300

    Initial Commit

---
+++ Flash Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Mykyta Havrylenko on 11.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
